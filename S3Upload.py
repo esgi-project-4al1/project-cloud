33fa3239f4df2436133b34c097251b09866397b7
Aristide Fumo <aristide.fumo@sopht.com@sopht.com>
2024-01-25 00:02:46 +0100

    solve error download file

--- S3Upload.py
+++ S3Upload.py
@@ -9,7 +9,7 @@ class S3Upload:
             "s3",
             aws_access_key_id=aws_access_key_id,
             aws_secret_access_key=aws_secret_access_key,
-            region_name='eu-north-1'
+            region_name='us-east-1'
         )
         self.url = ""
 
